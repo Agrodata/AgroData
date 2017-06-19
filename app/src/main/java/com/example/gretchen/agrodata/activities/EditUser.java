4628adcb1fec001b4a8e713e44492bd748c694c8
Agrodata <agronodata@gmail.com>
2017-06-18 21:36:18 -0400

    Sixth Commit

--- app/src/main/java/com/example/gretchen/agrodata/activities/EditUser.java
+++ app/src/main/java/com/example/gretchen/agrodata/activities/EditUser.java
@@ -32,71 +32,7 @@ public class EditUser extends ParentActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
 
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, SearchActivity.class)));
-        //Iconify the widget. Don't show search bar until search icon is pressed.
-        searchView.setIconifiedByDefault(true);
-        //Submit button not displayed by default
-        searchView.setSubmitButtonEnabled(true);
-
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    //On selecting action bar icons
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Take appropriate action for each action item click
-        switch (item.getItemId()) {
-            case R.id.action_search:
-                // search action
-                onSearchRequested();
-                return true;
-            case R.id.action_add_product:
-                //Add product
-                Intent addProduct = new Intent(this, AddProduct.class);
-                startActivity(addProduct);
-
-                return true;
-            case R.id.action_view_profile:
-                // View profile
-                Intent userProfile = new Intent(this, UserProfile.class);
-                //Get user info saved internally
-                SharedPreferences userInfo = getSharedPreferences(getString(R.string.login_preference_key), Context.MODE_PRIVATE);
-
-                //Sending user id to profile page.
-                userProfile.putExtra(getString(R.string.id_key),userInfo.getInt(getString(R.string.id_key),0));
-
-                startActivity(userProfile);
-
-
-                return true;
-            case R.id.action_log_out:
-                // Log out
-                SharedPreferences loginInfo = getSharedPreferences(getString(R.string.login_preference_key), Context.MODE_PRIVATE);
-                //This is so shared preference can be edited.
-                SharedPreferences.Editor loginEditor = loginInfo.edit();
-                //Set users id
-                loginEditor.putInt(getString(R.string.id_key),0);
-                //Save changes
-                loginEditor.commit();
-                //GO back to welcome page
-                Intent logout = new Intent(this, Welcome.class);
-                startActivity(logout);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
     //Place user info in the activity
     private void fillInUserInfo()
     {
